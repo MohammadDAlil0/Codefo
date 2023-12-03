b815e0d7c2ffcb2dc07236e693a4597e3373d536
Mohamad <mohmaddalil5@gmail.com>
2023-12-03 18:36:27 +0300

    initial step

---
+++ app.js
@@ -0,0 +1,45 @@
+//1 packages
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimiter = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
+//2 const variables
+const app = express();
+
+//3 midlewares
+
+//midlewares for testing
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+    app.use((req, res, next) => {
+        req.requestTime = new Date().toISOString();
+    });
+}
+
+//midleware to secure the express app
+app.use(helmet);
+
+//Limit requests from same IP
+const limiter = rateLimiter({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try after one houre'
+});
+app.use('/api', limiter);
+
+//body parser
+app.use(express.json({limit: '10KB'}));
+
+//Data sanitizaion NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against xss
+app.use(xss());
+
+//4 Routes
+
+
+module.exports = app;
